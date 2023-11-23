df9f33d49d0f9ee56e983a27aa9bffffce1b3063
kapellan422 <kapellan422@gmail.com>
2023-11-23 13:39:25 +0700

    closing the menu by clicking outside the menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
+    menuOverlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuOverlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
